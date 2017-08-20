f3b534e19a8abebdf49771a8058a3e87996fbf76
git4m2 <git4m2@users.noreply.github.com>
2017-08-20 09:47:22 -0400

    v2.0 - Added script for non-existent repositories

--- push.sh
+++ push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# added this git-bash script filename to "C:\Projects\RainingChain\HTML5 Games\.git\info\exclude"
-
 # Example Steps...
 # 1. Input version: 14.1
 # 2. Input description: OOPs! Refactor (end of video @ 14:10)
@@ -13,7 +11,7 @@
 # 8. git push origin v14.1
 
 # Note 7: If the origin does not yet exist, you may create it using cURL.
-# ex. curl -u 'git4m2' https://api.github.com/user/repos -d '{"name":"GitBash"}'
+# ex. curl -u "git4m2" https://api.github.com/user/repos -d "{ \"name\": \"GitBash\" }"
 # Remember to add the origin:
 # ex. git remote add origin https://github.com/git4m2/GitBash.git
 # Push existing repository:
@@ -73,7 +71,95 @@ else
 fi
 
 
+# PARSE LOCAL PROJECT DIRECTORY PATH INTO ARRAY
+#localProjectDirPath="C:/Projects/<PROJECTNAME>"
+#localProjectDirPath="fatal: Not a git repository (or any of the parent directories): .git"
+localProjectDirPath=`git rev-parse --show-toplevel 2>&1`
+IFS=':' read -r -a array <<< "$localProjectDirPath"
+localExists=${array[0]}
+
+# DETERMINE IF PROJECT EXISTS IN LOCAL REPOSITORY
+if [ "$localExists" = "fatal" ]; then
+    echo ""
+    echo "Create local Git repository."
+    git init
+fi
+
+
+# DETERMINE IF PROJECT EXISTS IN REMOTE REPOSITORY (i.e. GitHub)
+#Example...
+#From https://github.com/git4m2/<PROJECTNAME>.git
+#5380a81b26547fa5adb3ae05219a283be9ab87d8        HEAD
+#5380a81b26547fa5adb3ae05219a283be9ab87d8        refs/heads/master
+#cd212bcce27a117f00d99b47029ab6f701835cb9        refs/tags/v1.0
+#5380a81b26547fa5adb3ae05219a283be9ab87d8        refs/tags/v1.0^{}
+#Alternatively...
+#fatal: No remote configured to list refs from.
+remoteExists=`git ls-remote --exit-code 2>&1`
+IFS=':' read -r -a array <<< "$remoteExists"
+remoteExists=${array[0]}
+
+
+if [ "$remoteExists" = "fatal" ]; then
+    echo ""
+    echo "Remote project does \"not\" exist."
+    
+    # LOCAL PROJECT DIRECTORY PATH
+	localProjectDirPath=`git rev-parse --show-toplevel 2>&1`
+
+    echo ""
+    echo "Current Local Project Directory Path"
+    echo "$localProjectDirPath"
+
+    # PARSE LOCAL PROJECT DIRECTORY PATH INTO ARRAY
+    IFS='/' read -r -a array <<< "$localProjectDirPath"
+    arrayLength=${#array[@]}
+    projectName=${array[$arrayLength - 1]}
+    
+    echo ""
+    echo "Suggested Project Name: "
+    echo "$projectName"
+    
+    # CONFIRM PROJECT NAME
+    echo ""
+    read -p "Accept Suggested Project Name (y/n)? " respProjName
+    
+    if [ "$respProjName" != "y" ]; then
+        # CONFIRM ALTERNATE PROJECT NAME
+        echo ""
+        read -p "Type an alternate project name: " projectName
+        echo "Alternate Project Name: $projectName"
+        
+        echo ""
+        read -p "Is this correct (y/n)? " respAltProjName
+        
+        if [ "$respAltProjName" != "y" ]; then
+            echo ""
+            echo "Alternate project name confirmation failed."
+            exit 1
+        else
+            echo ""
+            echo "Alternate project name \"$projectName\" confirmed."
+        fi
+    else
+        echo ""
+        echo "Project name \"$projectName\" confirmed."
+    fi
+else
+    echo ""
+    echo "Remote project does exist."
+fi
+
+
 # PROCESSING
+username=`git config user.name`
+
+if [ "$remoteExists" = "fatal" ]; then
+    echo ""
+    echo "Create remote Git repository \"$projectName\" with account \"$username\"."
+    curl -u "$username" https://api.github.com/user/repos -d "{ \"name\": \"$projectName\" }"
+fi
+
 echo ""
 echo "Repository Status..."
 git status
@@ -93,14 +179,20 @@ echo "Tag Files (local repo)..."
 git tag -a "v$ver" -m "$ver"
 #git status
 
+if [ "$remoteExists" = "fatal" ]; then
+    echo ""
+    echo "Add project to remote repository..."
+    git remote add origin https://github.com/$username/$projectName.git
+fi
+
 echo ""
 echo "Push Files (remote repo)..."
-git push origin master
+git push -u origin master
 #git status
 
 echo ""
 echo "Push Tags (remote repo)..."
-git push origin "v$ver"
+git push origin "v$ver" --tags
 #git status
 
 echo ""
